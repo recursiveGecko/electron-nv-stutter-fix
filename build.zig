e7d4a66d53a8cda4881f2465f231017ab1e0c95a
recursiveGecko <1461520+recursiveGecko@users.noreply.github.com>
2023-07-13 13:28:35 +0200

    build: add `build-small` flag to build a ReleaseSmall version in addition to the regular build

--- build.zig
+++ build.zig
@@ -1,23 +1,13 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
-    // const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "electron-nv-stutter-fix",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = try std.zig.CrossTarget.parse(.{.arch_os_abi = "x86_64-windows-msvc"}),
-        .optimize = optimize,
-    });
-    exe.linkLibC();
-    exe.addIncludePath("lib/nvapi/R535-OpenSource");
+    const exe = try buildExe(b, "electron-nv-stutter-fix", optimize);
 
-    exe.addAnonymousModule("yazap", .{
-        .source_file = .{ .path = "lib/yazap/src/lib.zig" },
-    });
-
-    b.installArtifact(exe);
+    if (b.option(bool, "build-small", "Build a version with ReleaseSmall") orelse false) {
+        _ = try buildExe(b, "electron-nv-stutter-fix-small", .ReleaseSmall);
+    }
 
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
@@ -27,4 +17,25 @@ pub fn build(b: *std.Build) !void {
     }
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+}
+
+fn buildExe(
+    b: *std.Build,
+    name: []const u8,
+    optimize_mode: std.builtin.OptimizeMode,
+) !*std.Build.Step.Compile {
+    const exe = b.addExecutable(.{
+        .name = name,
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = try std.zig.CrossTarget.parse(.{ .arch_os_abi = "x86_64-windows-msvc" }),
+        .optimize = optimize_mode,
+    });
+    exe.linkLibC();
+    exe.addIncludePath("lib/nvapi/R535-OpenSource");
+    exe.addAnonymousModule("yazap", .{
+        .source_file = .{ .path = "lib/yazap/src/lib.zig" },
+    });
+
+    b.installArtifact(exe);
+    return exe;
 }
